f5e16ca258a398f7fc16d5a36d4c252c91bc1914
Einari Tuukkanen <einari.tuukkanen@gmail.com>
2018-04-12 14:01:47 +0300

    Adds detail modal

--- src/components/GroupData.vue
+++ src/components/GroupData.vue
@@ -7,19 +7,27 @@
                 <img class="rounded-circle" src="https://upload.wikimedia.org/wikipedia/fi/thumb/3/3b/Perussuomalaisten_logo.svg/1200px-Perussuomalaisten_logo.svg.png" alt="Generic placeholder image" width="140" height="140">
                 <h1 class="card-title pricing-card-title">62% <small class="text-muted">political</small></h1>
                 <p class="description">Your posts show that you identify strongly as being left based on your behaviour.</p>
-                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal1">
+                    View details
+                </button>    
+                
             </div><!-- /.col-lg-4 -->
             <div class="col-lg-4">
                 <img class="rounded-circle" src="https://1.gravatar.com/avatar/4c7368542982f0f081be48fc19e1fbfb?s=256&d=identicon&r=G" alt="Generic placeholder image" width="140" height="140">
                 <h1 class="card-title pricing-card-title">91% <small class="text-muted">favorite person</small></h1>
                 <p class="description">Very likely a partner or best friend. You keep in touch and connect more than with your other friends.</p>
-                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal2">
+                    View details
+                </button>    
+
             </div><!-- /.col-lg-4 -->
             <div class="col-lg-4">
                 <img class="rounded-circle" src="https://is2-ssl.mzstatic.com/image/thumb/Purple128/v4/ff/49/41/ff4941f3-cecd-9b9b-b1d7-b939b2ef045a/source/256x256bb.jpg" alt="Generic placeholder image" width="140" height="140">
                 <h1 class="card-title pricing-card-title">44% <small class="text-muted">sports team</small></h1>
                 <p class="description">It seems you're strongly into sports and your favorite team seems to be HIFK.</p>
-                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal3">
+                    View details
+                </button>                
             </div><!-- /.col-lg-4 -->
         </div>
         <br />
@@ -100,6 +108,180 @@
         <br />
         <br />
     </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Select data for removal</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            <ul class="list-group">
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <a data-toggle="collapse" href="#multiCollapseExample1" aria-expanded="false" aria-controls="multiCollapseExample1">
+                        2 photos
+                    </a>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" checked value="" id="defaultCheck1">
+                        <label class="form-check-label" for="defaultCheck1">
+                            
+                        </label>
+                    </div>
+                </li>
+                <div class="collapse" id="multiCollapseExample1">
+                    <div class="card card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <img class="list-img small" src="https://kuvat.uusisuomi.fi/sites/default/files/imagecache/artikkelikuva_std/kuvat/17.53164374.jpg">
+                                <div>
+                                    Olipa kiva puhe eilen Trumpilta, jäbä on äijä!
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" checked value="" id="defaultCheck2">
+                                    <label class="form-check-label" for="defaultCheck2">
+                                                                                
+                                    </label>
+                                </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <img class="list-img small" src="https://i.pinimg.com/originals/a4/a7/50/a4a7505287aeea786e8bba031c83a42b.jpg">
+                                <div>
+                                    Niin hyvää propagandaa.
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" checked value="" id="defaultCheck3">
+                                    <label class="form-check-label" for="defaultCheck3">
+                                                                                
+                                    </label>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <a data-toggle="collapse" href="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample1">
+                        4 posts
+                    </a>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" checked value="" id="defaultCheck4">
+                        <label class="form-check-label" for="defaultCheck4">
+                                                        
+                        </label>
+                    </div>
+                </li>
+                <div class="collapse" id="multiCollapseExample2">
+                    <div class="card card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <div>
+                                    Olin tänään kokouksessa.
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" checked value="" id="defaultCheck5">
+                                    <label class="form-check-label" for="defaultCheck5">
+                                                                                
+                                    </label>
+                                </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <div>
+                                    Niin hyvää propagandaa.
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" checked value="" id="defaultCheck6">
+                                    <label class="form-check-label" for="defaultCheck6">
+                                                                                
+                                    </label>
+                                </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <div>
+                                    Jytky!
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" checked value="" id="defaultCheck6">
+                                    <label class="form-check-label" for="defaultCheck6">
+                                                                                
+                                    </label>
+                                </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <div>
+                                    Oispa kaliaa.
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" checked value="" id="defaultCheck6">
+                                    <label class="form-check-label" for="defaultCheck6">
+                                                                                
+                                    </label>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <a data-toggle="collapse" href="#multiCollapseExample3" aria-expanded="false" aria-controls="multiCollapseExample1">
+                        3 likes
+                    </a>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" checked value="" id="defaultCheck7">
+                        <label class="form-check-label" for="defaultCheck7">
+                                                                    
+                        </label>
+                    </div>
+                </li>
+                <div class="collapse" id="multiCollapseExample3">
+                    <div class="card card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <div>
+                                    Trump
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" checked value="" id="defaultCheck8">
+                                    <label class="form-check-label" for="defaultCheck8">
+                                                                                
+                                    </label>
+                                </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <div>
+                                    Perussuomalaiset
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" checked value="" id="defaultCheck9">
+                                    <label class="form-check-label" for="defaultCheck9">
+                                                                                
+                                    </label>
+                                </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <div>
+                                    NATO
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" checked value="" id="defaultCheck9">
+                                    <label class="form-check-label" for="defaultCheck9">
+                                                                                
+                                    </label>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </ul>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Back</button>
+                <button type="button" class="btn btn-primary">Delete Selected</button>
+            </div>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
@@ -130,5 +312,16 @@ export default {
 }
 .list-img {
     margin: 0;
+}
+.list-img.small {
+    height: 100px;
+    padding-right: 15px;
+}
+.form-check-label {
+    font-size: 32px;
+}
+.form-check-input {
+    width: 20px;
+    height: 20px;
 }
 </style>
