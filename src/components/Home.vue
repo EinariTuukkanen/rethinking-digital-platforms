9d8cc1a9663139f368b145f72f1e47a7b6cd9c3f
Einari Tuukkanen <einari.tuukkanen@gmail.com>
2018-05-04 09:16:29 +0300

    Small changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,11 @@
 <template>
 <div>
   <div class="jumbotron text-center">
-    <h1>Welcome <span class="text-primary">You</span>.</h1>
-    <h4 class="text-muted">Go ahead, select a category.</h4>
+    <h1>Hello <span class="text-primary">You</span>.</h1>
+    <br />
+    <h4 class="text-muted">We analyzed your profile and our algorithms paired you with the following topics.</h4>
+    <br />
+    <h4 class="text-muted">Go ahead and select one.</h4>
     {{ message }}
   </div>
   <div class="container">
@@ -10,6 +13,9 @@
       <keyword-card v-for="kw in keywords" :key="kw.keyword" :data="kw"></keyword-card>
     </div>
   </div>
+  <br />
+  <br />
+  <br />
 </div>
 </template>
 
